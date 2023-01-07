d0055cd93c10d50c90fc1e3d9e9e7417303c2b14
Luke Carr <me+oss@carr.sh>
2023-01-07 18:07:40 +0000

    docs: migrated from vitepress to next.js (nextra)

---
+++ .docs/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    logo: <span>Mímir</span>,
+    project: {
+        link: 'https://github.com/subtalegames/mimir',
+    },
+    docsRepositoryBase: 'https://github.com/subtalegames/mimir/blob/main/.docs'
+}
